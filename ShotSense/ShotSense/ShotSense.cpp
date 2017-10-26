af11930f25dd5f43d5e8040ac7ecd09f33ca8ee2
ido <idoc8688@gmail.com>
2017-10-26 18:37:14 +0300

    Getting delta rotation from movement

--- ShotSense/ShotSense/ShotSense.cpp
+++ ShotSense/ShotSense/ShotSense.cpp
@@ -4,8 +4,18 @@
 #include "stdafx.h"
 #include <iostream>
 #include "ShotSense.h"
-
 int countWrist = 0;
+
+
+bool flag = true;
+quaternion initQuat;
+quaternion lastQuat;
+Acceleration lastAcc;
+
+
+void printQuat(const quaternion& q) {
+	cout << q.X << " " << q.Y << " " << q.Z << " " << q.W << endl;
+}
 
 void connectToSensors() {
 	Gem_Get(ARM_MAC_ADDR, &_handleArm);
@@ -16,18 +26,23 @@ void connectToSensors() {
 
 void OnCombinedDataWrist(unsigned short timestamp, const float* quaternion, const float* acceleration)
 {
+	if (flag) {
+		initQuat.set(quaternion::quaternion(quaternion[0], quaternion[1], quaternion[2], quaternion[3]));
+		flag = false;
+	}
+	lastQuat.set(quaternion::quaternion(quaternion[0], quaternion[1], quaternion[2], quaternion[3]));
 	if (countWrist++ == 100) {
-		std::cout << "data of Wrist\n";
-		std::cout << "timestamp: " << timestamp << "\n";
-		std::cout << "quaternion: " << quaternion[0] << ", " << quaternion[1] << ", " << quaternion[2] << ", " << quaternion[3] << "\n";
-		std::cout << "acceleration: " << acceleration[0] << ", " << acceleration[1] << ", " << acceleration[2] << "\n";
+		//std::cout << "data of Wrist\n";
+		//std::cout << "timestamp: " << timestamp << "\n";
+		//std::cout << "quaternion: " << quaternion[0] << ", " << quaternion[1] << ", " << quaternion[2] << ", " << quaternion[3] << "\n";
+		//std::cout << "acceleration: " << acceleration[0] << ", " << acceleration[1] << ", " << acceleration[2] << "\n";
 		countWrist = 0;
 	}
 }
 
 void OnTapDataWrist(unsigned int direction)
 {
-	std::cout << "OnTapData, direction: " << direction << "\n";
+	//std::cout << "OnTapData, direction: " << direction << "\n";
 }
 
 
@@ -82,6 +97,26 @@ void OnStateChangedWrist(GemState state) {
 	std::cout << endl;
 }
 
+void analyseMovement() {
+	cout << "analysing..." << endl;
+	cout << "center quaternion: ";
+	printQuat(initQuat);
+	cout << endl << "current quaternion: ";
+	printQuat(lastQuat);
+	cout << endl << endl;
+	quaternion inverseCenter(initQuat.makeInverse());
+	quaternion deltaRotation(inverseCenter*lastQuat);
+
+	printQuat(deltaRotation);
+	/*cout << endl << "delta rotation in euler: ";
+	vector3df euler;
+	deltaRotation.toEuler(euler);
+	cout << euler.X << " " << euler.Y << " " << euler.Z << endl;*/
+	
+}
+
+//-0.00838301 -0.692344 -0.0724388 0.717874
+
 int main()
 {
 	GemStatusCode err = Gem_Initialize();
@@ -89,8 +124,23 @@ int main()
 	if (err != GEM_SUCCESS) {
 		return 1;
 	}
+	char op;
 	connectToSensors();
-
+	while (true) {
+		cin >> op;
+		if (op == 'r') {
+			analyseMovement();
+		}
+		else if (op == 'i') {
+			initQuat.set(lastQuat);
+			cout << "new center: ";
+			printQuat(initQuat);
+			cout << endl;
+		}
+		else {
+			break;
+		}
+	}
 	getchar(); // prevent the program from terminating       
 	Gem_Terminate();
 	return 0;
--- ShotSense/ShotSense/ShotSense.h
+++ ShotSense/ShotSense/ShotSense.h
@@ -1,16 +1,29 @@
 #pragma once
 
 #include <GemSDK.h>
+#include <irrlicht.h>
+#include <vector>
 
-#define WRIST_MAC_ADDR "00:80:98:DC:9E:09"
-#define ARM_MAC_ADDR "00:80:98:DC:9E:46"
+#define WRIST_MAC_ADDR "00:80:98:DC:9E:46"
+#define ARM_MAC_ADDR "00:80:98:DC:9E:09"
 
+using namespace irr::core;
 using namespace std;
+
+class Acceleration {
+	float x, y, z;
+public:
+	Acceleration(float x, float y, float z) : x(x), y(y), z(z) {};
+	Acceleration() : x(0), y(0), z(0) {};
+};
 
 GemHandle _handleWrist;
 GemHandle _handleArm;
+
+void printQuat(const quaternion& q);
 
 void connectToSensors();
 void OnStateChangedArm(GemState state);
 void OnStateChangedWrist(GemState state);
 
+void analyseMovement();
